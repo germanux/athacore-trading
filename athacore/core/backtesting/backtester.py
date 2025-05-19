1232e02de8603bda54af78fae57d99b4a6166e6d
German Caballero <germanux@gmail.com>
2025-05-19 21:36:00 +0200

    ﻿Backtesting sin pulir

--- athacore/core/backtesting/backtester.py
+++ athacore/core/backtesting/backtester.py
@@ -1,36 +1,60 @@
 import pandas as pd
 
 
-def run_backtest(data: pd.DataFrame, signal_column: str = 'position', initial_cash: float = 10000.0) -> dict:
+def run_backtest(data: pd.DataFrame, signal_column, initial_cash: float = 10000.0) -> dict:
     """Ejecuta un backtest simple sobre datos con señales de compra/venta."""
 
     if signal_column not in data.columns:
         raise ValueError(f"Falta la columna '{signal_column}' en los datos.")
 
-    cash = initial_cash
-    position = 0
+    cash = float(initial_cash) 
+    actions_cuantity = 0
     trades = []
 
+    #data["fecha"]=data["fecha"].astype(str)
+    #data.to_dict(orient="records")
+
+    #print("DATA", data)
     for i in range(1, len(data)):
         signal = data.iloc[i][signal_column]
-        price = data.iloc[i]['Close']
+        price = data.iloc[i]['cierre']
 
-        if signal == 1 and cash > 0:  # Comprar
-            position = cash / price
+        #print(signal, price)
+        """
+        Se compra y vende todo
+        - Se puede comprar menos de uno?
+        - Máximo de cuanto comprar a la vez quizás?
+        """
+        # Comprar
+        if signal and cash > 0:
+            #print("Compra")
+            actions_cuantity = cash / price
             cash = 0
-            trades.append(('BUY', price, data.index[i]))
+            trades.append([i, 'BUY', price, cash, data.iloc[i]["fecha"]])
+            
+        # Vender
+        elif not signal and actions_cuantity > 0:
+            #print("Venta")
+            cash = actions_cuantity * price
+            actions_cuantity = 0
+            trades.append([i, 'SELL', price, cash, data.iloc[i]["fecha"]])
+            
+    #print(type(data['cierre'].iloc[-1]), type(actions_cuantity), type(cash))
 
-        elif signal == -1 and position > 0:  # Vender
-            cash = position * price
-            position = 0
-            trades.append(('SELL', price, data.index[i]))
+    final_cash = cash + actions_cuantity * data['cierre'].iloc[-1]
+    profit = final_cash - float(initial_cash)
 
-    final_value = cash + position * data['Close'].iloc[-1]
-    profit = final_value - initial_cash
+    trades_dicc =[{"indice": a, "compra": b, "price": c, "dinero": d, "fecha": e} for a, b, c, d, e in trades]
+    trades_df = pd.DataFrame(trades, columns=["indice", "compra", "price", "dinero", "fecha"])
 
-    return {
-        'initial_cash': initial_cash,
-        'final_value': round(final_value, 2),
-        'profit': round(profit, 2),
-        'trades': trades
+    #print(trades)
+
+    #return [initial_cash, final_cash, profit, trades]
+
+    estadisticas = {
+        'Dinero inicial': initial_cash,
+        'Dinero ganado': round(final_cash, 2),
+        'Ganancias': round(profit, 2),
     }
+
+    return estadisticas, trades_df
--- athacore/gui/tabs/backtesting_view.py
+++ athacore/gui/tabs/backtesting_view.py
@@ -1,35 +1,76 @@
 from nicegui import ui
 from athacore.core.strategy.strategy_engine import run_analysis
 from athacore.core.backtesting.backtester import run_backtest
+from athacore.core.strategy.strategy_engine import mostrar_estrategias, run_analysis
+
+import plotly.graph_objects as go
+
+ESTRATEGIAS = list(mostrar_estrategias().keys())
+
+def ejecutar_backtest(estrategia, ticker, start, end, initial_cash, status_label):
+        try:
+            señales = run_analysis(
+                strategy_name=estrategia.value,
+                ticker=ticker.value,
+                start=start.value,
+                end=end.value
+                )
+            #print(señales)
+
+            resultado, diccionario = run_backtest(señales, "compra", initial_cash.value)
+            #print(resultado)
+
+            with status_label:
+                with ui.row():
+                    for a, b in resultado.items():
+                         ui.label(f"{a.upper()}: {b}")
+                         
+                columna_graficas = ui.column()
+
+                with columna_graficas:
+                    def dibujar_grafica():
+                        columna_graficas.clear()
+
+                        fig = go.Figure()
+                        fig.add_trace(go.Scatter(
+                            x=diccionario["fecha"],
+                            y=diccionario["dinero"],
+                            mode='lines',
+                            name='Volumen'
+                        ))
+                        
+                        fig.update_layout(
+                            title=f"Crecimiento del dinero invertido",
+                            xaxis_title="Fecha",
+                            yaxis_title="Dinero",
+                            xaxis=dict(tickformat="%Y-%m-%d", type="date")
+                        )
+
+                        ui.plotly(fig).classes("w-full h-[600px]")
+                    dibujar_grafica()
+
+        except Exception as e:
+            with status_label:
+                 ui.label(f'Error: {e}')
 
 
 def render_backtesting_view():
-    with ui.column().classes('p-4'):
-        ticker_input = ui.input('Símbolo (ej: AAPL)').props('outlined')
-        estrategia_select = ui.select(
-            options=['estrategia_basica_medias', 'estrategia_rsi_simple', 'estrategia_macd_cruce'],
-            label='Estrategia',
-        )
-        status_label = ui.label()
-
-        def ejecutar_backtest():
-            try:
-                señales = run_analysis(
-                    strategy_name=estrategia_select.value,
-                    ticker=ticker_input.value,
-                    start="2023-01-01",
-                    end="2023-12-31"
+    with ui.row().classes("w-full gap-0"):
+            inputs = ui.column().classes("w-1/3")
+            with inputs:
+                estrategia = ui.select(
+                    options=ESTRATEGIAS,
+                    label='Selecciona una estrategia',
+                    value=ESTRATEGIAS[0]
                 )
-                resultado = run_backtest(señales)
+                ticker = ui.input('Ticker (ej: AAPL)', value="AAPL").props('outlined')
+                start = ui.input('Fecha inicio (YYYY-MM-DD)', value="2020-01-01").props('outlined')
+                end = ui.input('Fecha fin (YYYY-MM-DD)', value="2024-01-01").props('outlined')
+                initial_cash = ui.input('Dinero inicial', value="200").props('outlined')
 
-                resumen = f"""
-                💼 Valor inicial: {resultado['initial_cash']:.2f} €
-                📊 Valor final: {resultado['final_value']:.2f} €
-                📈 Ganancia total: {resultado['profit']:.2f} €
-                🔁 Nº de operaciones: {len(resultado['trades'])}
-                """
-                status_label.set_text(resumen.strip())
-            except Exception as e:
-                status_label.set_text(f'Error: {e}')
-
-        ui.button('Ejecutar Backtest', on_click=ejecutar_backtest)
+            output = ui.column().classes("w-2/3")
+            with output:
+                status_label = ui.column()
+            
+            ui.button('Ejecutar Backtest', on_click=lambda:ejecutar_backtest(estrategia, ticker, start, end, initial_cash, status_label))
+            
